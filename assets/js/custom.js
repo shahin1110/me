3bbd2289fe5218e55757d688aabb36a0e23fdf96
shahin1110 <shahinmail9@gmail.com>
2020-09-24 19:29:27 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('.nav-icon').click(function(){
+		$('.mobaile-menu').slideToggle(1000);
+		
+	});
+});
+
+$(document).ready(function(){
+			$(".btn-area").click(function(){
+				$(this).toggleClass("close-btn")
+			});
+		});
